eb1ab026ab27334dfab07b9cde552b46695f082c
flavio.cheung <flavio.cheung@gmail.com>
2026-03-10 08:40:29 +0100

    Initial Commit

---
+++ CoreMLApp/CoreMLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLAppApp.swift
+//  CoreMLApp
+//
+//  Created by Ihub Innopot on 10.03.26.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreMLAppTests/CoreMLAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLAppTests.swift
+//  CoreMLAppTests
+//
+//  Created by Ihub Innopot on 10.03.26.
+//
+
+import Testing
+@testable import CoreMLApp
+
+struct CoreMLAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
