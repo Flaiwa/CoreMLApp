b947664c5a2ccfcd1065b8ea49aef77f844f6d9f
flavio.cheung <flavio.cheung@gmail.com>
2026-04-12 10:20:36 +0200

    add Model info view

--- CoreMLApp/ContentView.swift
+++ CoreMLApp/ContentView.swift
@@ -11,12 +11,10 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                // Hintergrund passt sich automatisch an Dark/Light Mode an
                 Color(.systemBackground)
                     .ignoresSafeArea()
  
                 VStack(spacing: 32) {
-                    // App-Icon / Titel
                     Image(systemName: "camera.viewfinder")
                         .font(.system(size: 150))
                         .foregroundStyle(.green)
